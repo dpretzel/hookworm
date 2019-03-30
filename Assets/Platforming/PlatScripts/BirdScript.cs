b1c8a2780fe0b388275d51aa506d7e0e5c608edf
oakieland <48298728+oakieland@users.noreply.github.com>
2019-03-29 23:45:53 -0500

    Added multiple assets, cleaned up asset heirarchy
    Added:
    * Assets I had worked on from previous games
    
    * Cleaned up our folders
    
    * Created a new level
    
    * Created a new Player prefab
    
    * Created a script for birds so they aren't randomly colored
    
    * Added Snail sprite
    
    * Added bird sprite
    
    * Changed grapple texture

---
+++ Assets/Platforming/PlatScripts/BirdScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public float lift = -1;
+
+    private void Start()
+    {
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = -lift;
+    }
+	
+	void OnBecameVisible()
+    {
+        enabled = true;
+    }
+	
+}
